60f4931085e0ffbd6067a5efb4fb3d35b275e393
Quoc Huan Tran <quoc.huan.tran26@gmail.com>
2022-10-05 10:17:09 -0400

    Mise en place, et un login window test simple

--- Magix/index.php
+++ Magix/index.php
@@ -1,12 +1,30 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-</head>
-<body>
-    <div>HELLO</div>
-</body>
-</html>
+<?php
+    require_once("partial/header.php");
+
+
+?>
+
+
+
+
+<form id="loginform" action="index.php" method="post">
+
+    <div class="form-label">Utilisateur : </div>
+    <div class="form-input"><input type="email" name="courriel" /></div>
+    <div class="form-separator"></div>
+    
+    <div class="form-label">Mot de passe : </div>
+    <div class="form-input"><input type="password" name="motDePasse" /></div>
+    <div class="form-separator"></div>
+    
+    <div class="form-label">&nbsp;</div>
+    <div class="form-input"><button>Connexion</button></div>
+    <div class="form-separator"></div>
+
+
+</form>
+
+
+
+<?php
+    require_once("partial/footer.php");
