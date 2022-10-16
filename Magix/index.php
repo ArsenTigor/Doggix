b3675d2cd5ea51fbea8e063efecd200a0d28f99e
ArsenTigor <quoc.huan.tran26@gmail.com>
2022-10-16 13:13:14 -0400

    Asset and city

--- Magix/index.php
+++ Magix/index.php
@@ -7,32 +7,35 @@
 
     require_once("partial/header.php");
 ?>
+<div id=indextop>
+    <div id="titlebox">
+        <div id="title">Doggix</div>
+        <div id="subtitle">Where all doggo are good doggo</div>
+    </div>
 
-<div id="titlebox">
-    <div id="title">Doggix</div>
-    <div id="subtitle">Where all doggo are good doggo</div>
+    <form id="loginform" action="index.php" method="post">
+        <div class="form-label">Utilisateur : </div>
+        <div class="form-input"><input type="text" name="username"></div>
+        <div class="form-separator"></div>
+        
+        <div class="form-label">Mot de passe : </div>
+        <div class="form-input"><input type="password" name="password" /></div>
+        <div class="form-separator"></div>
+        
+        <div class="form-label">&nbsp;</div>
+        <div class="form-input"><button>Connexion</button></div>
+        <div class="form-separator"></div>
+    </form>
 </div>
 
-<form id="loginform" action="index.php" method="post">
-    <div class="form-label">Utilisateur : </div>
-    <div class="form-input"><input type="text" name="username"></div>
-    <div class="form-separator"></div>
-    
-    <div class="form-label">Mot de passe : </div>
-    <div class="form-input"><input type="password" name="password" /></div>
-    <div class="form-separator"></div>
-    
-    <div class="form-label">&nbsp;</div>
-    <div class="form-input"><button>Connexion</button></div>
-    <div class="form-separator"></div>
-</form>
 
 <div id="conteneur">
-    <div id="ciel">
-    </div>
 
-    <div id="terrain">
+    <div id="uppersection">
+        <div id="city"></div>
     </div>
+    
+    <div id="terrain"></div>
 
 </div>
 
