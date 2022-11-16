b4ba0e72dc0f2344947e71ebf4b9942e74ef4b96
Quoc Huan Tran <quoc.huan.tran26@gmail.com>
2022-11-16 11:09:27 -0500

    Jeu look

--- Magix/jeu.php
+++ Magix/jeu.php
@@ -11,14 +11,23 @@
     require_once("partial/header.php");
 ?>
 
-    <div id="waiting">WAITING</div>
-    <div id="gamewon">YOU WON</div>
-    <div id="gamelost">YOU LOST</div>
+<div id="waiting">WAITING</div>
+<div id="gamewon">YOU WON</div>
+<div id="gamelost">YOU LOST</div>
+
+<div id="backgroundtree"></div>
 
 <div id="conteneurmain">
-    
+    <div id="backgroundground"></div>
+    <div id="backgroundsky"></div>
+        
     <div id="opponentzone">
-        <div class="stats">
+        <div class="stats2">
+            <div id="opponentname">OPPONENT NAME</div>
+            <div id="opponentclass">OPPONENT CLASS</div>
+        </div>
+
+        <div class="stats2">
             <div id="opponentlife">LIFE</div>
             <div id="opponentenergy">ENERGY </div>
             <div id="opponentcardleft">CARD LEFT</div>
@@ -26,10 +35,10 @@
 
         <div id="opponentavatar"></div>
 
-        <div class="stats2">
-            <div id="opponentname">OPPONENT NAME</div>
-            <div id="opponentclass">OPPONENT CLASS</div>
-        </div>
+        <div id="extrawindow1">Add some enemy quote here</div>
+        <div id="extrawindow2">Add API reply to not good command</div>
+
+
     </div>
 
 
@@ -39,31 +48,31 @@
             <div id="deckpile"></div>
         </div>
         
-        <div id="opponentfield" class="cardzone"></div>
-        
-        <div id="spacer1"></div>
-        
-        <div id="playerfield" class="cardzone"></div>
+     
         
         <div id="rightplayzone">
             <div id="currentplayer"></div>
             <div class="timeranimation"></div>
             <div id="timer"></div>
         </div>
-        
-        <div id="spacer2"></div>
     
         <div class="stats">
             <div id="playerlife">LIFE</div>
             <div id="playerenergy">ENERGY </div>
         </div>
 
-        <div id="playerhand" class="cardzone"></div>
+        <div id="middlesection">
+            <div id="opponentfield" class="cardzone"></div>
+            
+            <div id="playerfield" class="cardzone"></div>
+
+            <div id="playerhand" class="cardzone"></div>
+        </div>
         
         <div id="menu">
             <div id="heropower"></div>
             <div id="endturn"></div>
-            <div id="surrender">Surrender</div>
+            <div id="surrender"></div>
         </div>
     </div>
 
